3aee7e642250b24a5c78a0bdfe09120ca4b20b47
Alefir Ulfhiemon <alefir@cock.li>
2019-01-25 01:07:45 -0800

    add python script to plot csv

---
+++ controlsoftware/plot.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+
+data = np.genfromtxt('test.csv', delimiter=',', names = True)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(data['time'], data['tc1'], label='tc1')
+ax1.plot(data['time'], data['tc2'], label='tc2')
+ax1.plot(data['time'], data['tc3'], label='tc3')
+ax1.plot(data['time'], data['tc4'], label='tc4')
+ax1.plot(data['time'], data['tc5'], label='tc5')
+ax1.plot(data['time'], data['tc6'], label='tc6')
+ax1.plot(data['time'], data['tc7'], label='tc7')
+ax1.plot(data['time'], data['tc8'], label='tc8')
+ax1.plot(data['time'], data['pt1'], label='pt1')
+ax1.plot(data['time'], data['pt2'], label='pt2')
+plt.xlabel('Time (ms)')
+plt.ylabel('Value')
+plt.legend(loc='best')
+plt.grid(True)
+plt.show()
