e388bf398dac294d62f96fcfcf10e77978fe3dad
Alefir Ulhiemon <alefir@cock.li>
2019-09-06 09:29:25 -0700

    Add board selection column header to data preparation script
    In response to 45e6ac8, in which a new column representing the status of
    board_selection is added to the log. This commit adds a new column
    header for that data so it can be handled correctly by plot.py

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -3,4 +3,4 @@ import sys
 filename = sys.argv[1]
 
 with open(filename, 'r') as original: data = original.read()
-with open(filename, 'w') as modified: modified.write("TIME,CH4,LOX,HEL,CBR,REG,UAF,HALLCH4,HALLLOX,BAT\n" + data)
+with open(filename, 'w') as modified: modified.write("TIME,BRD,CH4,LOX,HEL,CBR,REG,UAF,HALLCH4,HALLLOX,BAT\n" + data)
